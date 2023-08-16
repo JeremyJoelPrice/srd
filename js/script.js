bb11d8d31ee42b0118fcbacbdc9e8b67413f38a3
Matt <cunning.distraction@googlemail.com>
2023-08-17 00:05:01 +0100

    refactor parseJsonAsHTML method to switch case

--- js/script.js
+++ js/script.js
@@ -7,48 +7,52 @@ loadArticle("eldoria");
 */
 
 function loadArticle(articleName) {
-	const request = new XMLHttpRequest();
-	request.onreadystatechange = () => {
-		if (request.readyState == 4 && request.status == 200) {
-			document.querySelector(".article-container").innerHTML =
-				parseJsonAsHtml(request.responseText);
-		}
-	};
-	request.open("GET", `../json/${articleName}.json`);
-	request.send(null);
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = () => {
+    if (request.readyState == 4 && request.status == 200) {
+      document.querySelector(".article-container").innerHTML = parseJsonAsHtml(
+        request.responseText
+      );
+    }
+  };
+  request.open("GET", `../json/${articleName}.json`);
+  request.send(null);
 }
 
 function parseJsonAsHtml(json) {
-	const articleObj = JSON.parse(json);
-	let articleHtml = "";
+  const articleObj = JSON.parse(json);
+  let articleHtml = "";
 
-	// header
-	articleHtml += `<h1 class="header-text">${articleObj.header}</h1>`;
+  // header
+  articleHtml += `<h1 class="header-text">${articleObj.header}</h1>`;
 
-	// subheaders & paragraphs
-	articleObj.sections.forEach(({ type, content }) => {
-		if (type === "subheader") {
-			articleHtml += `<h2 class="subheader-text">${content}</h2>`;
-		} else if (type === "paragraph") {
-			content = replaceKeywordsWithHyperlinks(content);
-			articleHtml += `<p class="body-text">${content}</p>`;
-		}
-	});
+  // subheaders & paragraphs
+  articleObj.sections.forEach(({ type, content }) => {
+    switch (type) {
+      case "subheader":
+        articleHtml += `<h2 class="subheader-text">${content}</h2>`;
+        break;
+      case "paragraph":
+        content = replaceKeywordsWithHyperlinks(content);
+        articleHtml += `<p class="body-text">${content}</p>`;
+        break;
+    }
+  });
 
-	// article tag
-	articleHtml = `<article class="article">${articleHtml}</article>`;
+  // article tag
+  articleHtml = `<article class="article">${articleHtml}</article>`;
 
-	return articleHtml;
+  return articleHtml;
 }
 
 function replaceKeywordsWithHyperlinks(bodyText) {
-	Object.keys(map).forEach((keyword) => {
-		bodyText = bodyText.replaceAll(
-			keyword,
-			`<a href="${map[keyword]}" class="link--styled">${keyword}</a>`
-		);
-	});
-	return bodyText;
+  Object.keys(map).forEach((keyword) => {
+    bodyText = bodyText.replaceAll(
+      keyword,
+      `<a href="${map[keyword]}" class="link--styled">${keyword}</a>`
+    );
+  });
+  return bodyText;
 }
 
 /*
@@ -56,16 +60,11 @@ function replaceKeywordsWithHyperlinks(bodyText) {
 */
 
 document.addEventListener("click", (event) => {
-	// handle anchor tags
-	if (
-		event.target.matches(".link") ||
-		event.target.matches(".link--styled")
-	) {
-		event.preventDefault();
-		let href = event.target.href.substring(
-			event.target.href.lastIndexOf("/")
-		);
-		loadArticle(href);
-		scrollTo(0, 0);
-	}
+  // handle anchor tags
+  if (event.target.matches(".link") || event.target.matches(".link--styled")) {
+    event.preventDefault();
+    let href = event.target.href.substring(event.target.href.lastIndexOf("/"));
+    loadArticle(href);
+    scrollTo(0, 0);
+  }
 });
