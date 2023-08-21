3e916fcb4579c34c1c283cd76e24270b2d7d91c3
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2023-08-21 19:57:26 +0100

    add support for images in articles

--- js/hyperlink-map.js
+++ js/hyperlink-map.js
@@ -1,5 +1,6 @@
 export default {
 	Eldoria: "eldoria",
 	"Whispering Woods": "whispering-woods",
-	"Fountain of Dreams": "fountain-of-dreams"
+	"Fountain of Dreams": "fountain-of-dreams",
+	Astraloria: "astraloria"
 };
--- js/script.js
+++ js/script.js
@@ -1,92 +1,99 @@
-import map from "./hyperlink-map.js";
+import map from './hyperlink-map.js';
 
-loadArticle("eldoria");
+loadArticle('eldoria');
 
 /*
 	loading articles
 */
 
 function loadArticle(articleName) {
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = () => {
-    if (request.readyState == 4 && request.status == 200) {
-      document.querySelector(".article-container").innerHTML = parseJsonAsHtml(
-        request.responseText
-      );
-    }
-  };
-  request.open("GET", `../json/${articleName}.json`);
-  request.send(null);
+	const request = new XMLHttpRequest();
+	request.onreadystatechange = () => {
+		if (request.readyState == 4 && request.status == 200) {
+			document.querySelector('.article-container').innerHTML =
+				parseJsonAsHtml(request.responseText);
+		}
+	};
+	request.open('GET', `../json/${articleName}.json`);
+	request.send(null);
 }
 
 function parseJsonAsHtml(json) {
-  const articleObj = JSON.parse(json);
-  let articleHtml = "";
+	const articleObj = JSON.parse(json);
+	let articleHtml = '';
 
-  // header
-  articleHtml += `<h1 class="header-text">${articleObj.header}</h1>`;
+	// header
+	articleHtml += `<h1 class="header-text">${articleObj.header}</h1>`;
 
-  // subheaders & paragraphs
-  articleObj.sections.forEach(({ type, content }) => {
-    switch (type) {
-      case "subheader":
-        articleHtml += `<h2 class="subheader-text">${content}</h2>`;
-        break;
-      case "paragraph":
-        content = replaceKeywordsWithHyperlinks(content);
-        articleHtml += `<p class="body-text">${content}</p>`;
-        break;
-      case "table":
-        articleHtml += parseCsvAsHtmlTable(content);
-    }
-  });
+	// other sections
+	articleObj.sections.forEach(({ type, content }) => {
+		switch (type) {
+			case 'subheader':
+				articleHtml += `<h2 class="subheader-text">${content}</h2>`;
+				break;
+			case 'paragraph':
+				content = replaceKeywordsWithHyperlinks(content);
+				articleHtml += `<p class="body-text">${content}</p>`;
+				break;
+			case 'table':
+				articleHtml += parseCsvAsHtmlTable(content);
+				break;
+			case 'image':
+				articleHtml += `<img class="article__image" src=${content} />`;
+		}
+	});
 
-  // article tag
-  articleHtml = `<article class="article">${articleHtml}</article>`;
+	// article tag
+	articleHtml = `<article class="article">${articleHtml}</article>`;
 
-  return articleHtml;
+	return articleHtml;
 }
 
 function replaceKeywordsWithHyperlinks(bodyText) {
-  Object.keys(map).forEach((keyword) => {
-    bodyText = bodyText.replaceAll(
-      keyword,
-      `<a href="${map[keyword]}" class="link--styled">${keyword}</a>`
-    );
-  });
-  return bodyText;
+	Object.keys(map).forEach((keyword) => {
+		bodyText = bodyText.replaceAll(
+			keyword,
+			`<a href="${map[keyword]}" class="link--styled">${keyword}</a>`
+		);
+	});
+	return bodyText;
 }
 
 function parseCsvAsHtmlTable(csv) {
-  let html = "";
+	let html = '';
 
-  const rows = csv.split("\n");
+	const rows = csv.split('\n');
 
-  for (let i = 0; i < rows.length; i++) {
-    html += `<tr>`;
-    rows[i].split(",").forEach((cell) => {
-      const cellTags =
-        i === 0
-          ? [`<th class="table__cell">`, "</th>"]
-          : [`<td class="table__cell">`, "</td>"];
-      html += `${cellTags[0]}${cell}${cellTags[1]}`;
-    });
-    html += `</tr>`;
-  }
+	for (let i = 0; i < rows.length; i++) {
+		html += `<tr>`;
+		rows[i].split(',').forEach((cell) => {
+			const cellTags =
+				i === 0
+					? [`<th class="table__cell">`, '</th>']
+					: [`<td class="table__cell">`, '</td>'];
+			html += `${cellTags[0]}${cell}${cellTags[1]}`;
+		});
+		html += `</tr>`;
+	}
 
-  return `<table class="table">${html}</table>`;
+	return `<table class="table">${html}</table>`;
 }
 
 /*
 	event listeners
 */
 
-document.addEventListener("click", (event) => {
-  // handle anchor tags
-  if (event.target.matches(".link") || event.target.matches(".link--styled")) {
-    event.preventDefault();
-    let href = event.target.href.substring(event.target.href.lastIndexOf("/"));
-    loadArticle(href);
-    scrollTo(0, 0);
-  }
+document.addEventListener('click', (event) => {
+	// handle anchor tags
+	if (
+		event.target.matches('.link') ||
+		event.target.matches('.link--styled')
+	) {
+		event.preventDefault();
+		let href = event.target.href.substring(
+			event.target.href.lastIndexOf('/')
+		);
+		loadArticle(href);
+		scrollTo(0, 0);
+	}
 });
